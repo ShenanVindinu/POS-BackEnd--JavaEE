caecb2ab8da45c84e142768d81401579c12f4db9
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-22 15:47:14 +0530

    init created!

--- src/main/java/org/example/posbackendjavaee/Controller/CustomerController.java
+++ src/main/java/org/example/posbackendjavaee/Controller/CustomerController.java
@@ -5,9 +5,26 @@ import jakarta.servlet.http.HttpServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @WebServlet(urlPatterns = "/Customer", loadOnStartup = 2)
 public class CustomerController extends HttpServlet {
     static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    Connection connection;
 
+    public void init() {
+        logger.info("Initializing StudentController with call init method");
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/POS-BackEnd");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
