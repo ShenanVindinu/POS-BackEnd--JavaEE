50f76d4cd932d33986f8e5d47cdd733054f01d5d
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-22 15:57:06 +0530

    DoPost content check created

--- src/main/java/org/example/posbackendjavaee/Controller/CustomerController.java
+++ src/main/java/org/example/posbackendjavaee/Controller/CustomerController.java
@@ -2,12 +2,15 @@ package org.example.posbackendjavaee.Controller;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -25,6 +28,15 @@ public class CustomerController extends HttpServlet {
         } catch (NamingException | SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (!req.getContentType().toLowerCase().startsWith("application/json") || req.getContentType() == null) {
+            //send error
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+
     }
 
 }
