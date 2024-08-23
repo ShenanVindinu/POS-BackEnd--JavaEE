45bb89d85e8e242c9159edaf401304c1e49c0268
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-23 15:37:05 +0530

    CustomerDTO updated

--- src/main/java/org/example/posbackendjavaee/model/CustomerDTO.java
+++ src/main/java/org/example/posbackendjavaee/model/CustomerDTO.java
@@ -1,4 +1,14 @@
 package org.example.posbackendjavaee.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CustomerDTO {
+    private String name;
+    private String address;
+    private String salary;
 }
