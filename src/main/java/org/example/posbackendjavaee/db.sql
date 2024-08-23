0f209508246d2750f3bb6dd5eee914ce95fad277
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-23 22:41:00 +0530

    new query added for item table creation

--- src/main/java/org/example/posbackendjavaee/db.sql
+++ src/main/java/org/example/posbackendjavaee/db.sql
@@ -4,3 +4,11 @@ create table customer(
                         address VARCHAR(20),
                         salary VARCHAR(50)
 )
+
+create table item(
+                         id VARCHAR(50) primary key,
+                         name VARCHAR(50),
+                         qty VARCHAR(20),
+                         price VARCHAR(50)
+)
+
