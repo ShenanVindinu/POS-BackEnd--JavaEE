526a67194d0e15c729e1a1fd0c710a3c5844b007
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-23 16:13:08 +0530

    customer table query added

---
+++ src/main/java/org/example/posbackendjavaee/db.sql
@@ -0,0 +1,6 @@
+create table customer(
+                        id VARCHAR(50) primary key,
+                        name VARCHAR(50),
+                        address VARCHAR(20),
+                        salary VARCHAR(50)
+)
