22d21bfb2ec0d1051c72de6863d8d18973da3927
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-23 16:21:56 +0530

    getCustomer method added

--- src/main/java/org/example/posbackendjavaee/dao/CustomerDataProcessImpl.java
+++ src/main/java/org/example/posbackendjavaee/dao/CustomerDataProcessImpl.java
@@ -1,4 +1,35 @@
 package org.example.posbackendjavaee.dao;
 
-public class CustomerDataProcessImpl {
+import org.example.posbackendjavaee.model.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public final class CustomerDataProcessImpl {
+
+    static String SAVE_STUDENT = "INSERT INTO student (id,name,city,email,level) VALUES (?,?,?,?,?)";
+    static String GET_STUDENT = "SELECT * FROM customer WHERE id=?";
+    static String UPDATE_STUDENT = "UPDATE student SET name=?,city=?,email=?,level=? WHERE id=?";
+    static String DELETE_STUDENT = "DELETE FROM student WHERE id=?";
+
+    public CustomerDTO getStudentById(String id, Connection connection) throws SQLException {
+        var customerDTO = new CustomerDTO();
+        try {
+            var ps = connection.prepareStatement(GET_STUDENT);
+            ps.setString(1, id);
+            var resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                customerDTO.setId(resultSet.getString("id"));
+                customerDTO.setName(resultSet.getString("name"));
+                customerDTO.setAddress(resultSet.getString("address"));
+                customerDTO.setSalary(resultSet.getString("salary"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return customerDTO;
+    }
+
+
 }
+
