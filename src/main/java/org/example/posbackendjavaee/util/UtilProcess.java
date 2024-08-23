ee026882d922417d9f87aa7c8684e51c71c4db08
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-23 15:44:49 +0530

    util created to generate id

---
+++ src/main/java/org/example/posbackendjavaee/util/UtilProcess.java
@@ -0,0 +1,11 @@
+package org.example.posbackendjavaee.util;
+
+import java.util.UUID;
+
+public class UtilProcess {
+
+    public static String generateId() {
+        return UUID.randomUUID().toString();
+    }
+
+}
