804e62269b24989c27e493ddbc774ef17a28e86a
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-27 15:41:50 +0530

    Order Servlet Created

---
+++ src/main/java/org/example/posbackendjavaee/Controller/OrderController.java
@@ -0,0 +1,14 @@
+package org.example.posbackendjavaee.Controller;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = "Order", loadOnStartup = 2)
+public class OrderController extends HttpServlet {
+
+
+
+
+}
