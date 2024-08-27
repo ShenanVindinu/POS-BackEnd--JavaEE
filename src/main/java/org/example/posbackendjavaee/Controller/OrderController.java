c4b106a4419ffa0fa34b7e076ee0a7b6d5fd1641
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-27 15:43:49 +0530

    Init Method Created Logs Implemented

--- src/main/java/org/example/posbackendjavaee/Controller/OrderController.java
+++ src/main/java/org/example/posbackendjavaee/Controller/OrderController.java
@@ -4,11 +4,30 @@ import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @WebServlet(urlPatterns = "Order", loadOnStartup = 2)
 public class OrderController extends HttpServlet {
+    static Logger logger = LoggerFactory.getLogger(CustomerController.class);
+    Connection connection;
 
-
+    public void init(ServletConfig config) throws ServletException {
+        logger.info("Initializing OrderController with call init method");
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/POS-BackEnd");
+            this.connection = pool.getConnection();
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
